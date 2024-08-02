c3101073676d65000e038c4ba282b5c18aef4a7b
Siddharth <sahusidd715@gmail.com>
2024-08-02 17:04:43 +0530

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/javascripts/src.js 	new file:   views/index.ejs

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
- const express=require("express")
- const app=express()
- const users=require("./MOCK_DATA.json")
- app.listen(8000,()=>{
+const { name } = require("ejs");
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/users/:name", (req, res) => {
+    let name = req.params.name;
+    res.send(`Hello ${name}`);
+});
 
- })
+app.listen(3000, () => {
+  console.log("Its running");
+});
